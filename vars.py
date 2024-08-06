d0575a87e397b13a86fe0fda8a7c883016bb8ffd
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-06 11:07:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "22620068"))
+API_HASH = environ.get("API_HASH", "11e2c113078324f7e36688baa86c3911")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
